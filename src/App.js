258196430920782cec5e9051858737775dbeb595
pingusl <pingusl@yahoo.fr>
2022-05-04 14:10:08 +0200

    Create Header with vinted logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Header from "./containers/header";
 import Home from "./containers/home";
 import "./App.css";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Link to="/home">Go to Home</Link>
       <Routes>
         <Route path="/home" element={<Home />}></Route>
---
+++ src/containers/header.js
@@ -0,0 +1,13 @@
+import logo from "../logo.svg";
+const Header = () => {
+  return (
+    <header className="header">
+      <span>
+        <img src={logo} className="logo" alt="logo-vinted" />
+      </span>
+
+      <span></span>
+    </header>
+  );
+};
+export default Header;
