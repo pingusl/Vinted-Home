f274ba5749f19a72ec8462cdaef70e42ec676b1c
pingusl <pingusl@yahoo.fr>
2022-05-04 13:50:15 +0200

    Create Route Home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./containers/home";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
