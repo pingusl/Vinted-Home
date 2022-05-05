9fcfc9674a2d25197a07d86762e540920d8af9ab
pingusl <pingusl@yahoo.fr>
2022-05-05 14:38:01 +0200

    CLEANING: App.js

--- src/App.js
+++ src/App.js
@@ -4,38 +4,15 @@ import Home from "./containers/home";
 import Offer from "./containers/offer";
 import NoMatch from "./containers/nomatch";
 import "./App.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
 
 function App() {
-  //----Create states for manage data----//
-  const [isLoading, setIsLoading] = useState(true); //To be sur data will be loading
-  const [data, setData] = useState(); //To record the data
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://lereacteur-vinted-api.herokuapp.com/offers"
-        );
-        // console.log(response.data.offers[0]._id);
-        setData(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  return isLoading === true ? (
-    <h1>En cours de chargement 🥁</h1>
-  ) : (
+  return (
     <Router>
       <Header />
       <Link to="/">Go to Home</Link>
       <Link to="/offer">Go to Offer</Link>
       <Routes>
-        <Route path="/" element={<Home data={data} />} />
+        <Route path="/" element={<Home />} />
         <Route path="/offer/:id" element={<Offer />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
