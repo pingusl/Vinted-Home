282d9f6a7a139aeebdb5b4a13d52bc81bd4539a6
pingusl <pingusl@yahoo.fr>
2022-05-05 15:04:27 +0200

    Add signup form

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -1,4 +1,35 @@
+import { Link } from "react-router-dom";
 const Signup = () => {
-  return <div className="signup-container">Signup</div>;
+  return (
+    <div className="signup-container">
+      {" "}
+      <form id="form-contact">
+        <h1>S'inscrire</h1>
+        <input
+          type="text"
+          placeholder="Nom utilisateur"
+          name="username"
+          id="username"
+        />
+
+        <input type="email" placeholder="Email" name="email" id="email" />
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          name="password"
+          id="password"
+        />
+        <legend>
+          <checkbox>S'inscrire à note newsletter</checkbox>
+        </legend>
+        <button type="submit" value="signup">
+          S'inscrire
+        </button>
+        <Link to="/signin">
+          <p className="signup-text">Tu as déjà un compte? Connecte-toi!</p>
+        </Link>
+      </form>
+    </div>
+  );
 };
 export default Signup;
