d11bbc2f6b8b727becd24f60d89e74e2ed54b2e0
pingusl <pingusl@yahoo.fr>
2022-05-07 12:39:41 +0200

    PUT CSS on  Elements in search bar

--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -11,20 +11,26 @@ const Header = () => {
         <img src={logo} className="logo-img" alt="logo-vinted" />
       </span>
       <span className="search">
-        <div className="search-bt">
-          <img className="search-img" src={magnifyingGlass} alt="search" />
-          <input
-            className="search"
-            type="search"
-            onChange={(event) => {
-              setSearchInput(event.target.value);
-            }}
-            value={searchInput}
-          />
-          <button className="clear-bt" type="button">
-            X
-          </button>
-        </div>
+        <form className="search-bar" id="#search-bar">
+          <div className="search-img">
+            <img className="search-img" src={magnifyingGlass} alt="search" />
+          </div>
+          <div className="search-input">
+            <input
+              className="search-input"
+              type="text"
+              onChange={(event) => {
+                setSearchInput(event.target.value);
+              }}
+              value={searchInput}
+            />
+          </div>
+          <div className="search-clear">
+            <button className="clear-bt" type="button">
+              X
+            </button>
+          </div>
+        </form>
       </span>
       <span className="button-group">
         <Link className="sign-bt" to="/signup">
