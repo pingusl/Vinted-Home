417cf2ef0dc8f3c2033001cb31bb28104e445edf
pingusl <pingusl@yahoo.fr>
2022-05-07 10:44:45 +0200

    PUT Element in search bar

--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -1,14 +1,30 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import logo from "../../logo.svg";
+import magnifyingGlass from "../../img/loupe.svg";
 import "./header.scss";
 const Header = () => {
+  const [searchInput, setSearchInput] = useState("");
   return (
     <header className="header">
       <span className="logo">
         <img src={logo} className="logo-img" alt="logo-vinted" />
       </span>
       <span className="search">
-        <input className="search" type="search" />
+        <div className="search-bt">
+          <img className="search-img" src={magnifyingGlass} alt="search" />
+          <input
+            className="search"
+            type="search"
+            onChange={(event) => {
+              setSearchInput(event.target.value);
+            }}
+            value={searchInput}
+          />
+          <button className="clear-bt" type="button">
+            X
+          </button>
+        </div>
       </span>
       <span className="button-group">
         <Link className="sign-bt" to="/signup">
