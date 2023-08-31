2667e7d1565020d513bc12cf338aab7cb3693d11
Sébastien Lefebvre <87710689+pingusl@users.noreply.github.com>
2023-08-31 17:08:54 +0200

    Improve home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,19 +1,16 @@
-import { Link, useNavigate } from "react-router-dom";
-import bannerImg from "../../img/banner-hero.jpeg";
-import tornImg from "../../img/effet-déchiré.png";
+import { useNavigate } from "react-router-dom";
+import tear from "../../img/tear.svg";
 import { RotatingLines } from "react-loader-spinner";
+import Card from "../../components/cards";
 
-import "./home.scss";
+import "../../assets/styles/home.css";
 
 function Home({ data, isLoading }) {
-  //----Create states for manage data----//
-  // console.log(token);
-  // const [data, setData] = useState(); //To record the data
   const navigate = useNavigate();
 
-  // console.log(isLoading);
   return isLoading === true ? (
     <RotatingLines
+      className="home-loader"
       strokeColor="green"
       strokeWidth="5"
       animationDuration="0.75"
@@ -21,62 +18,29 @@ function Home({ data, isLoading }) {
       visible={true}
     />
   ) : (
-    <div className="home-container">
-      <div className="banner">
-        <div className="selling-now">
-          <h1>Prêts à faire du tri dans vos placards?</h1>
-          <button
-            className="green-bt selling-bt"
-            onClick={() => {
-              navigate("/publish");
-            }}
-          >
-            Vends maintenant
-          </button>
-          <p className="pop-text">Découvrir comment ça marche</p>
-        </div>
-        <div className="wrapper">
-          <div className="banner">
-            <img className="banner" src={bannerImg} alt="hero" />
-          </div>
-          <div className="tornImg">
-            <img className="tornImg" src={tornImg} alt="torn" />
+    <>
+      <div className="home-hero-bg-img">
+        <img src={tear} alt="forme" className="home-hero-forme" />
+        <div>
+          <div className="home-hero-ready">
+            Prêts à faire du tri dans vos placards?
+            <button
+              onClick={() => {
+                navigate("/publish");
+              }}
+            >
+              Vends maintenant
+            </button>
           </div>
         </div>
       </div>
-
-      <div>
-        <h2>Articles populaires</h2>
+      <div className="home-card-wrapper">
+        {data.offers &&
+          data.offers.map((card, index) => {
+            return <Card key={index} data={card} />;
+          })}
       </div>
-      <section className="offers">
-        {data.offers
-          ? data.offers.map((offer, index) => {
-              return (
-                <div className="offer" key={index}>
-                  <Link to={`offer/${offer._id}`}>
-                    <img
-                      className="offer-img"
-                      src={offer.product_image.secure_url}
-                      alt={offer._id}
-                    />
-                  </Link>
-                  <div className="offer-details">
-                    <span className="offer-article-details"></span>
-                    <div className="article-price">
-                      {offer.product_price}&nbsp;€
-                    </div>
-                    <div className="article-size"></div>
-                    <div className="article-brand">
-                      {offer.product_details[0].MARQUE}
-                    </div>
-                    <span className="offer-like"></span>
-                  </div>
-                </div>
-              );
-            })
-          : null}
-      </section>
-    </div>
+    </>
   );
 }
 
